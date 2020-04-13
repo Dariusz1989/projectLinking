0d77d0e3c59ef86797525af91ab9af5d3e12faeb
Dariusz1989 <dariusz1989@gmail.com>
2020-04-13 20:07:24 +0100

    shish

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Master on 13/04/2020.
+//
+
+#include "projectB/src/extendedTreeView.h"
+#include "coreProject/icLib/icEntity/entity.h"
+
+int main(){
+    entity en;
+    extendedTreeView view;
+    return 10;
+}
